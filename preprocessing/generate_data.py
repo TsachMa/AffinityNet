5cf00beb7950431cd902423309cace92a27f26ff
Tsach <tsach276@mit.edu>
2024-06-20 23:23:27 -0400

    more test data and pocket generator

--- preprocessing/generate_data.py
+++ preprocessing/generate_data.py
@@ -5,6 +5,7 @@ import os
 import argparse
 
 from src.data.parse_data import create_cleaned_dataset
+from src.data.add_H_and_mol2_chimera import add_H_and_convert_to_mol2
 
 def main(): 
     #take as an argument the path to your data
@@ -19,6 +20,8 @@ def main():
     output_name = os.path.join(args.PDB_data_path, 'cleaned_dataset.csv')
 
     create_cleaned_dataset(PDBbind_data_path, general_set_PDBs_path, refined_set_PDBs_path, output_name, plot = True)
+    add_H_and_convert_to_mol2(general_set_PDBs_path, pocket_files=False, cli=True)
+    add_H_and_convert_to_mol2(refined_set_PDBs_path, pocket_files=False, cli=True)
 
 if __name__ == '__main__':
     main()
--- src/utils/constants.py
+++ src/utils/constants.py
@@ -1,3 +1,6 @@
 import os 
 
-PROJECT_ROOT = os.path.abspath("../../")
+#get the directory of the current file
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+# get the root directory of the project (2 levels up from the current file)
+PROJECT_ROOT = os.path.abspath(os.path.join(CURRENT_DIR, os.pardir, os.pardir))
